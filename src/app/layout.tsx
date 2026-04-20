2decc4a235d764f09a615855332ab12e7ebbd9ad
prashant00797 <prashantnath6307@gmail.com>
2026-04-20 17:35:12 +0530

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter, Space_Grotesk } from "next/font/google";
 import "../global.css";
 import { type ReactNode } from "react";
-
+import Script from "next/script";
 const interSans = Inter({
   variable: "--font-sans",
   subsets: ["latin"],
@@ -26,7 +26,21 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       lang="en"
       className={`${interSans.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
-      <body className="min-h-full">{children}</body>
+      <body className="min-h-full">
+        {children}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HZ5W9ELQBV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-HZ5W9ELQBV');
+  `}
+        </Script>
+      </body>
     </html>
   );
 }
